733eb891b10735c7c3900b09e208860d9c934ebf
ezhh <owlecho@live.com>
2017-02-28 20:54:57 +0000

    Add recipes for when caverealms is present

---
+++ crafting.lua
@@ -0,0 +1,28 @@
+
+-- recipes when caverealms is present
+if minetest.get_modpath("caverealms") then
+
+	local recipe_list = {
+		{"black", "spike",}, {"blue", "glow_crystal",},
+		{"cyan", "glow_gem",}, {"green", "glow_emerald",}, 
+		{"magenta", "salt_gem",}, {"orange", "fire_vine",}, 
+		{"purple", "glow_amethyst",}, {"red", "glow_ruby",}, 
+		{"yellow", "glow_mese",}, {"white", "glow_ore",},
+	}
+
+	for _, item in pairs(recipe_list) do
+
+		local colour = item[1]
+		local ingredient = item[2]
+
+		minetest.register_craft({
+			output = "abritorch:torch_"..colour.." 4", 
+			recipe = {
+				{"default:torch", "", "default:torch" },
+				{"", "caverealms:"..ingredient, "" },
+				{"default:torch", "", "default:torch" },
+			}
+		})
+	end
+
+end
